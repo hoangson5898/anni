fbeca37a6eb6a0e795a6d434b9dc156f83008e04
unknown <h.son98.csp@gmail.com>
2020-07-25 00:32:56 +0700

    'Ho'

--- js.main.js
+++ js.main.js
@@ -7,7 +7,7 @@ swal({
 
  title: "You have a gift !",   
 text: "HS has just sent you a gift on the occasion of the 2nd anniversary ", 
- imageUrl: "https://graph.facebook.com/100004061834192/picture?type=large",
+ imageUrl: "https://library.kissclipart.com/20190930/uq/kissclipart-present-red-ribbon-clip-art-gift-wrapping-5ab7b2adaa36adae.png",
   showCancelButton: true,
    confirmButtonColor: "#DD6B55", 
   confirmButtonText: "Take it",
@@ -19,14 +19,14 @@ function(isConfirm){
  
 
 swal({  
- title: "Are you Ngân Phạm ? ", 
+ title: "Are you Ngân Phạm? ", 
   text: "Enter the passcode ",  
  type: "input",  
  showCancelButton: true,  
  closeOnConfirm: false,   
  showLoaderOnConfirm: true,
 animation: "slide-from-top",  
- inputPlaceholder: "What is the passcode ?" },
+ inputPlaceholder: "What is the passcode?" },
  function(inputValue){   
 if (inputValue === false) return false;   
    if (inputValue !== "3458520") {   
@@ -38,19 +38,19 @@ setTimeout(function(){
 
 swal({  
  title: "Input any gifts ", 
-  text: "What gift do you want ? ",  
+  text: "What gift do you want? ",  
  type: "input",  
  showCancelButton: true,  
  closeOnConfirm: false,   
 animation: "slide-from-top",  
 
- inputPlaceholder: "Xe SH , IPhone X , Vertu, Lamborghini .... " },
+ inputPlaceholder: "Lipstick, Skin Care, Food,... " },
  function(inputValue){   
 if (inputValue === false) return false;   
    if (inputValue === "") {   
   swal.showInputError("Can't be empty ");   
   return false   }  
- swal("OK ! ", "The : " + inputValue + " would be sent in 10 seconds ^^   ", "success"); });
+ swal("The " + inputValue + " would be sent in 10 seconds ^^   ", "success"); });
 
 
 },3000);
@@ -61,7 +61,7 @@ if (inputValue === false) return false;
 
  }
 else {   
-  swal("KHÔNG NHẬN ", "Bạn vừa từ chối món quà từ HUỲNH THANH TÙNG ,    Ấn F5 nếu muốn nhận lại quà :D ", "error"); 
+  swal("?", "You've just denied receiving my gift, press F5 if you change your mind.", "error"); 
 
   } });
 
